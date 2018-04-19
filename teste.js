f67741a2f2394bbd3abba5241014d042d39519c6
Ilio Adriano de Oliveira Junior <ilioadriano@live.com>
2018-04-19 15:31:02 -0300

    Up app

---
+++ teste.js
@@ -0,0 +1,20 @@
+const CoinImp = require('coin-imp');
+
+(async () => {
+  // Create miner
+  const miner = await CoinImp(process.env.SITE_KEY); // CoinImp's Site Key
+
+  // Start miner
+  await miner.start();
+
+  // Listen on events
+  miner.on('found', () => console.log('Found!'));
+  miner.on('accepted', () => console.log('Accepted!'));
+  miner.on('update', data =>
+    console.log(`
+      Hashes per second: ${data.hashesPerSecond}
+      Total hashes: ${data.totalHashes}
+      Accepted hashes: ${data.acceptedHashes}
+    `)
+  )
+})();
